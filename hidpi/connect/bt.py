df16b10778b7de847ead27893456f317edf554db
Heerkog <heerkogroefsema@gmail.com>
2019-12-14 19:19:40 +0100

    Bluetooth

--- hidpi/connect/bt.py
+++ hidpi/connect/bt.py
@@ -1,22 +1,25 @@
 import bluetooth
 
-class advertiser:
+
+class Advertiser:
     uuid = "ec20ee5f-491d-4f9c-adb6-26250bdcfbd1"
+
+    def __init__(self):
+        self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
     def advertise(self):
         try:
-            self.server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-            self.port = bluetooth.get_available_port( bluetooth.RFCOMM )
-            self.server_sock.bind(("", self.port))
+            port = bluetooth.get_available_port(bluetooth.RFCOMM)
+            self.server_sock.bind(("", port))
             self.server_sock.listen(1)
-            print("listening on port %d" % self.port)
-            bluetooth.advertise_service( self.server_sock, "FooBar Service", self.uuid, [0x0508] )
+            print("listening on port %d" % port)
+            bluetooth.advertise_service(self.server_sock, "FooBar Service", self.uuid, [0x0508])
         except:
             print("Failed to advertise Bluetooth HID service")
 
     def accept(self):
-        client_sock,address = self.server_sock.accept()
-        print("Accepted connection from ",address)
+        client_sock, address = self.server_sock.accept()
+        print("Accepted connection from ", address)
         return client_sock
 
     def disconnect(self, client_sock):
