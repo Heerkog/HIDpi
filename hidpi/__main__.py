9114abfb25c79cb706a90434ba8b5442bc188f68
Heerkog <heerkogroefsema@gmail.com>
2019-12-15 14:33:48 +0100

    Bluetooth

--- hidpi/__main__.py
+++ hidpi/__main__.py
@@ -1,8 +1,11 @@
 from hidpi.connect import bt
+import time # Used for pausing the process
+
 
 if __name__ == '__main__':
     adv = bt.Advertiser()
     adv.advertise()
     client = adv.accept()
+    time.sleep(5)
     adv.disconnect(client)
     adv.close()
--- hidpi/connect/bt.py
+++ hidpi/connect/bt.py
@@ -3,17 +3,16 @@ import bluetooth
 
 class Advertiser:
     uuid = "ec20ee5f-491d-4f9c-adb6-26250bdcfbd1"
+    port = 3
 
     def __init__(self):
         self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
     def advertise(self):
-        port = bluetooth.get_available_port(bluetooth.RFCOMM)
-        print("Failed to create port")
-        self.server_sock.bind(("B8:27:EB:77:31:44", port))
+        self.server_sock.bind(("B8:27:EB:77:31:44", self.port))
         print("bound")
         self.server_sock.listen(1)
-        print("listening on port %d" % port)
+        print("listening on port %d" % self.port)
         bluetooth.advertise_service(self.server_sock, "FooBar Service", self.uuid, [0x0508])
         print("advertising")
 
