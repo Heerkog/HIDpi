5c60fdd30b546bfb10bb6032ec8bb4131564d2c3
Heerkog <heerkogroefsema@gmail.com>
2019-12-15 14:46:15 +0100

    Bluetooth

--- hidpi/connect/bt.py
+++ hidpi/connect/bt.py
@@ -3,17 +3,16 @@ import bluetooth
 
 class Advertiser:
     uuid = "ec20ee5f-491d-4f9c-adb6-26250bdcfbd1"
-    port = 3
 
     def __init__(self):
         self.server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
     def advertise(self):
-        self.server_sock.bind(("B8:27:EB:77:31:44", self.port))
+        self.server_sock.bind(("B8:27:EB:77:31:44", bluetooth.PORT_ANY))
         print("bound")
         self.server_sock.listen(1)
-        print("listening on port %d" % self.port)
-        bluetooth.advertise_service(self.server_sock, "FooBar Service", self.uuid, ["0508"])
+        print("listening on port %d" % self.server_sock.getsockname()[1])
+        bluetooth.advertise_service(self.server_sock, "FooBar Service", self.uuid, [self.uuid, 0x0508])
         print("advertising")
 
     def accept(self):
