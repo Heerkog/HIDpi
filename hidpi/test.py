4b699c1e26b8bb2e9763c2be0f014398c5ba1c44
Heerkog <heerkogroefsema@gmail.com>
2019-12-15 19:16:33 +0100

    Bluetooth

--- hidpi/test.py
+++ hidpi/test.py
@@ -2,8 +2,7 @@ import bluetooth
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-port = bluetooth.get_available_port( bluetooth.RFCOMM )
-server_sock.bind(("",port))
+server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
 print("listening on port %d" % port)
 
