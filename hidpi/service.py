447eb2394bd9a75d5b2e4a803ee5f76a8727989b
Heerkog <heerkogroefsema@gmail.com>
2019-12-21 11:00:09 +0100

    Dbus registration

--- hidpi/service.py
+++ hidpi/service.py
@@ -6,26 +6,16 @@ import dbus.mainloop.glib
 from gi.repository import GLib as glib
 from hidpi.hid import Joystick
 import socket
-import bluetooth
 
 global mainloop
 
 #define a bluez 5 profile object for our keyboard
 class BluezProfile(dbus.service.Object):
-    MY_ADDRESS = "B8:27:EB:77:31:44"
-    interrupt_port = 19  #HID interrupt port as specified in SDP > Additional Protocol Descriptor List > L2CAP > HID Interrupt Port
     file_descriptor = -1
-    control_channel = None
     interrupt_channel = None
 
-    def __init__(self, bus, path):
-        dbus.service.Object.__init__(self, bus, path)
-
-        self.interrupt_socket = bluetooth.BluetoothSocket(bluetooth.L2CAP)  #socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_L2CAP)
-        self.interrupt_socket.setblocking(0)
-        self.interrupt_socket.bind(("", self.interrupt_port))
-        self.interrupt_socket.listen(1)
-        glib.io_add_watch(self.interrupt_socket.fileno(), glib.IO_IN, self.accept_interrupt)
+    def __init__(self, bus, name, path):
+        dbus.service.Object.__init__(self, name, path)
 
     @dbus.service.method("org.bluez.Profile1", in_signature="", out_signature="")
     def Release(self):
@@ -39,7 +29,7 @@ class BluezProfile(dbus.service.Object):
     @dbus.service.method("org.bluez.Profile1", in_signature="oha{sv}", out_signature="")
     def NewConnection(self, path, file_descriptor, properties):
         self.file_descriptor = file_descriptor.take()
-        self.control_channel = socket.fromfd(file_descriptor, socket.AF_BLUETOOTH, socket.SOCK_STREAM)
+        self.interrupt_channel = socket.fromfd(file_descriptor, socket.AF_BLUETOOTH, socket.SOCK_STREAM)
 
         print("NewConnection(%s, %d)" % (path, self.file_descriptor))
 
@@ -49,7 +39,7 @@ class BluezProfile(dbus.service.Object):
             else:
                 print("  %s = %s" % (key, properties[key]))
 
-        glib.io_add_watch(self.control_channel, glib.PRIORITY_DEFAULT, glib.IO_IN | glib.IO_PRI, self.control_callback)
+        glib.io_add_watch(self.interrupt_channel, glib.PRIORITY_DEFAULT, glib.IO_IN | glib.IO_PRI, self.interrupt_channel)
 
     @dbus.service.method("org.bluez.Profile1", in_signature="o", out_signature="")
     def RequestDisconnection(self, path):
@@ -58,13 +48,6 @@ class BluezProfile(dbus.service.Object):
         if (self.file_descriptor > 0):
             os.close(self.file_descriptor)
             self.file_descriptor = -1
-            self.control_channel.close()
-            self.interrupt_channel.close()
-
-    def control_callback(self, file_descriptor, conditions):
-        data = os.read(file_descriptor, 1024)
-        print("{0}".format(data))
-        return True
 
     def interrupt_callback(self, file_descriptor, conditions):
         data = os.read(file_descriptor, 1024)
@@ -77,19 +60,11 @@ class BluezProfile(dbus.service.Object):
         except ConnectionResetError:
             self.file_descriptor = -1
 
-    def accept_interrupt(self, source, cond):
-        self.interrupt_channel, cinfo = self.interrupt_socket.accept()
-        glib.io_add_watch(self.interrupt_channel, glib.PRIORITY_DEFAULT, glib.IO_IN | glib.IO_PRI, self.io_callback)
-        print("Got a connection on the interrupt channel from " + cinfo[0])
-        return True
-
 #create a bluetooth device to emulate a HID joystick
 class BTJoystick:
-    MY_ADDRESS = "B8:27:EB:77:31:44"
     MY_DEV_NAME = "RPi_HID_Joystick"
-    control_port = 17  #HID control port as specified in SDP > Protocol Descriptor List > L2CAP > HID Control Port
-    interrupt_port = 19  #HID interrupt port as specified in SDP > Additional Protocol Descriptor List > L2CAP > HID Interrupt Port
     PROFILE_DBUS_PATH = "/nl/rug/ds/heerkog/hid"  #dbus path of the bluez profile
+    PROFILE_DBUS_NAME = "nl.rug.ds.heerkog.hid"  #dbus mame of the bluez profile
     SDP_RECORD_PATH = sys.path[0] + "/sdp/sdp_record_joystick.xml"  #file path of the sdp record to laod
     UUID = "00001124-0000-1000-8000-00805f9b34fb"  #HumanInterfaceDeviceServiceClass UUID
 
@@ -116,8 +91,12 @@ class BTJoystick:
         adapter_properties.Set('org.bluez.Adapter1', 'Pairable', dbus.Boolean(1))
         adapter_properties.Set('org.bluez.Adapter1', 'Discoverable', dbus.Boolean(1))
 
+
+        system_bus.request_name(self.PROFILE_DBUS_NAME)
+        bus_name = dbus.service.BusName(self.PROFILE_DBUS_NAME, system_bus)
+        self.profile = BluezProfile(system_bus, bus_name, self.PROFILE_DBUS_PATH)
+
         profile_manager = dbus.Interface(system_bus.get_object("org.bluez", "/org/bluez"), "org.bluez.ProfileManager1")
-        self.profile = BluezProfile(system_bus, self.PROFILE_DBUS_PATH)
         profile_manager.RegisterProfile(self.PROFILE_DBUS_PATH, self.UUID, opts)
 
         print("Profile registered")
