85d9403f5ae643dc31359aae933efcb6aa0fa222
Heerkog <heerkogroefsema@gmail.com>
2019-12-22 11:38:16 +0100

    Back to ports

--- hidpi/sock_test.py
+++ hidpi/sock_test.py
@@ -19,9 +19,10 @@ if __name__ == '__main__':
     print("Setting up connections")
 
     control_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_L2CAP)
-    control_socket.bind((MY_ADDRESS, control_port))
     control_socket.setblocking(0)
     control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    control_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    control_socket.bind((MY_ADDRESS, control_port))
     control_socket.listen(1)
 
     glib.io_add_watch(control_socket.fileno(), glib.IO_IN, accept_control)
