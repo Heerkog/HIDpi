f5532b1ef1d807af269eff9ccbbbd5155d49467e
Heerkog <heerkogroefsema@gmail.com>
2019-12-23 14:08:03 +0100

    Back to ports

--- hidpi/sock_test.py
+++ hidpi/sock_test.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     if not os.geteuid() == 0:
         sys.exit("Only root can run this script")
 
-    MY_ADDRESS = "b8:27:eb:77:31:44"
+    MY_ADDRESS = "B8:27:EB:77:31:44"
     control_port = 19  #HID control port as specified in SDP > Protocol Descriptor List > L2CAP > HID Control Port
 
     print("Setting up connections")
