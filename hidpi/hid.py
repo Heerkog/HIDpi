8d139b15215aeb61c8365f2360ad4db09660cb1e
Heerkog <heerkogroefsema@gmail.com>
2019-12-27 12:06:40 +0100

    Initial tests

--- hidpi/hid.py
+++ hidpi/hid.py
@@ -15,11 +15,11 @@ class HumanInterfaceDevice(object):
     def send_report(self):
         print("x: {0} y: {1} b: {2}".format(self.state[1], self.state[2], self.state[3]))
 
-        report = ""
+        bytes = bytearray()
         for val in self.state:
-            report += chr(val)
+            bytes.append(val.to_bytes(1, byteorder='big', signed=True))
 
-        self.report_function(report)
+        self.report_function(bytes)
 
         return True
 
