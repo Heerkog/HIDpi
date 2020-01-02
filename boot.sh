92b74106b9cecf30fbea5805c0dbb6a43d5451a9
Heerkog <heerkogroefsema@gmail.com>
2020-01-02 14:14:05 +0100

    Initial version

--- boot.sh
+++ boot.sh
@@ -5,7 +5,7 @@ sudo /etc/init.d/bluetooth stop &>/dev/null &
 sleep 5
 sudo service bluetooth stop &>/dev/null &
 sleep 5
-sudo /usr/sbin/bluetoothd --nodetach --debug &>/dev/null &
+sudo /usr/sbin/bluetoothd --nodetach --compat --debug -p time &>/dev/null &
 
 # Wait for the service to start
 sleep 10
