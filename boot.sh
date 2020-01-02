f8a55bddd33b5b71fe97dee84b010fef29970b6f
Heerkog <heerkogroefsema@gmail.com>
2020-01-02 13:58:29 +0100

    Initial version

--- boot.sh
+++ boot.sh
@@ -1,14 +1,14 @@
 # Manually stop and start the Bluetooth service
 sudo service dbus restart &>/dev/null &
-sleep 1
+sleep 5
 sudo /etc/init.d/bluetooth stop &>/dev/null &
-sleep 1
+sleep 5
 sudo service bluetooth stop &>/dev/null &
-sleep 2
+sleep 5
 sudo /usr/sbin/bluetoothd --nodetach --debug &>/dev/null &
 
 # Wait for the service to start
-sleep 2
+sleep 5
 
 # Start the Python script
 sudo python -m hidpi
